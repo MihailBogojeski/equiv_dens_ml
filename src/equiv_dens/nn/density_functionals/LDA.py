fixed errors with softmax constraint and energy functional

src/equiv_dens/nn/density_functionals/LDA.py
@@ -10,7 +10,7 @@ class LDAFunctional(nn.Module):
         super().__init__()
         self.a_num = a_num
         self.use_PME = use_PME
-        self.ewald = Ewald(a_num, PME=use_PME)
+        self.ewald = Ewald(a_num, PME=use_PME, verbose=verbose)
         self.verbose = verbose
 
     def forward(self, rho, grid, pos, pseudo_pot):

src/equiv_dens/nn/density_functionals/ewald.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class Ewald(nn.Module):
     def __init__(
-        self, a_num, PME=False, prec=1.0e-8, eta=None
+        self, a_num, PME=False, prec=1.0e-8, eta=None, verbose=0
     ):
         super().__init__()
         self.a_num = a_num
@@ -14,6 +14,7 @@ class Ewald(nn.Module):
         self.prec = prec
         self.eta = eta
         self.mask = None
+        self.verbose = verbose
 
     def forward(self, rho, grid, pos):
         gmax = self.get_gmax(grid)
@@ -22,11 +23,12 @@ class Ewald(nn.Module):
             self.eta = self.get_best_eta(gmax)
         # print('self eta', self.eta)
         self.real_en = self.real_energy(rho, grid, pos)
-        # print("real energy", self.real_en)
         self.corr_en = self.corr_energy(rho, grid, pos)
-        # print("corr energy", self.corr_en)
         self.rec_en = self.rec_energy(rho, grid, pos)
-        # print("rec energy", self.rec_en)
+        if self.verbose > 0:
+            print("Ewald real energy", self.real_en)
+            print("Ewald corr energy", self.corr_en)
+            print("Ewald rec energy", self.rec_en)
         ewald_en = self.real_en + self.corr_en + self.rec_en
 
         return ewald_en
@@ -36,6 +38,7 @@ class Ewald(nn.Module):
         prec = sp.erfcinv(self.prec / 3.0)
         rmax = prec / np.sqrt(self.eta)
         N = torch.ceil(rmax / L)
+        print('L', L)
 
         charges = []
         positions = []
@@ -53,12 +56,11 @@ class Ewald(nn.Module):
                         positions.append(pos[i] - R)
 
         esum = 0.0
-        rtol = 0.001
+        rtol = 0.01
         rcut = rmax
         eta_sqrt = np.sqrt(self.eta)
         positions = torch.stack(positions)
         charges = torch.tensor(charges).to(rho)
-
         for i in range(len(self.a_num)):
             dists = torch.cdist(positions, pos[i].view((1, 3))).view(-1)
             index = torch.logical_and(dists < rcut, dists > rtol)

src/equiv_dens/nn/modules/spherical_harmonics_expansion.py
@@ -163,7 +163,7 @@ class SphericalHarmonicsExpansion(nn.Module):
                 L0_coeffs_comb = F.softmax(L0_coeffs_comb, dim=1)
                 L0_coeffs_comb = L0_coeffs_comb * self.n_electrons
             else:
-                coeffs_sum = torch.sum(L0_coeffs_comb, dim=1)
+                coeffs_sum = torch.sum(L0_coeffs_comb, dim=1, keepdim=True)
                 scale_factor = self.n_electrons / coeffs_sum
                 L0_coeffs_comb = L0_coeffs_comb * scale_factor
         coeffs_pointer = 0

src/equiv_dens/train_dens_func.py
@@ -88,7 +88,7 @@ print("loading atoms from" + args.np_dataset + "...")
 # density_file = '/home/mihail/data/water_rot/full_densities.hdf5'
 # np_file = 'h2o_overlap_static.npy'
 grid_origin = -2.0318
-cube_grid_fn = partial(cubical_grid, nx=20, ny=20, nz=20,
+cube_grid_fn = partial(cubical_grid, nx=args.cube_size, ny=args.cube_size, nz=args.cube_size,
                        extent=np.array([4.1483, 4.1483, 4.1483]),
                        origin=np.array([-2.0318, -2.0318, -2.0318]))
 cube_sampling_fn = cubical_sampling
@@ -356,7 +356,7 @@ while step < args.max_steps + 1:
     for i in range(predictions['density'].shape[0]):
         pseudo_pot.restart(grid=grid, ions=data['ions'][i])
         en_preds.append(lda(predictions['density'][i].view((args.cube_size, args.cube_size, args.cube_size)),
-                            grid_cl, data['positions'][i] - grid_origin, pseudo_pot))
+                            grid_cl, du.angstrom_to_bohr(data['positions'][i] - grid_origin), pseudo_pot))
     print('energy preds', en_preds)
     predictions['energy'] = torch.stack(en_preds).to(predictions['density'])
 
@@ -439,7 +439,7 @@ while step < args.max_steps + 1:
                 for i in range(predictions['density'].shape[0]):
                     pseudo_pot.restart(grid=grid, ions=data['ions'][i])
                     en_preds.append(lda(predictions['density'][i].view((args.cube_size, args.cube_size, args.cube_size)),
-                                    grid_cl, data['positions'][i] - grid_origin, pseudo_pot))
+                                    grid_cl, du.angstrom_to_bohr(data['positions'][i] - grid_origin), pseudo_pot))
                 predictions['energy'] = torch.stack(en_preds).to(predictions['density'])
 
                 print('val density integral', torch.sum(predictions['density'] * data['coord_weights'], dim=-1))

src/equiv_dens/training/grids.py
@@ -2,17 +2,17 @@ import numpy as np
 from pyscf.dft import gen_grid, radi
 from .rotation import random_rotation_matrix
 from pyscf import lib
-from pyscf.lib import param
 import torch
 from dftpy.math_utils import bestFFTsize
 from dftpy.grid import DirectGrid
+import equiv_dens.utils.dft_utils as du
 
 
 def spherical_grid(mols, level=2):
     print('level', level)
     grid_spec = gen_grid.gen_atomic_grids(mols[0], radi_method=radi.treutler_ahlrichs, level=level)
     for key in grid_spec.keys():
-        grid_spec[key] = (grid_spec[key][0] * param.BOHR, grid_spec[key][1])  # convert Bohr grid to Angstrom
+        grid_spec[key] = (grid_spec[key][0] * du.to_angstrom, grid_spec[key][1])  # convert Bohr grid to Angstrom
 
     return grid_spec
 
@@ -40,7 +40,7 @@ def cubical_grid(mols, nx=125, ny=125, nz=125, resolution=None,
 
     sample_volume = extent / np.array([nx, ny, nz])
 
-    sample_volume /= param.BOHR
+    sample_volume *= du.to_bohr
     sample_volume = np.prod(sample_volume)
 
     coords = lib.cartesian_prod([xs, ys, zs])