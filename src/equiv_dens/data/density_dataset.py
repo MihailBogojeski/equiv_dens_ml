4f3d3f951c71c65d419960dc79f54b643d320b6a
MihailBogojeski <mihailbogojeski@gmail.com>
2021-01-22 02:12:10 +0100

    setting up for running ethanol

--- src/equiv_dens/data/density_dataset.py
+++ src/equiv_dens/data/density_dataset.py
@@ -84,8 +84,8 @@ class AtomsDensityData(Dataset):
         self.coeffs = []
         self.ions = []
         ase_atoms = utils.npy_to_ase(self.atoms['shifted_positions'], self.atoms['atom_types'])
-        for i in range(10):
-        # for i in range(len(calc_results)):
+        # for i in range(10):
+        for i in range(self.atoms['positions'].shape[0]):
             if self.density_path is not None:
                 mol_dict, calc_dict = calc_results[i]
                 coeff_dict = {'mo_coeff': calc_dict['mo_coeff'], 'mo_occ': calc_dict['mo_occ']}
