18c1742b43f8bc0819a1f3b82477c683e74ba7ad
MihailBogojeski <mihailbogojeski@gmail.com>
2021-09-06 20:12:21 +0200

    implemented representation energy networks, testing now

--- src/equiv_dens/nn/property_output/energy.py
+++ src/equiv_dens/nn/property_output/energy.py
@@ -648,3 +648,287 @@ class SimpleEnergyNetworkv2(nn.Module):
         if self.timing:
             print('simple energy time', time.time() - start)
         return atoms
+
+
+class RepresentationEnergyNetwork(nn.Module):
+    """
+    Neural network for computing the energy of a molecule based on the density coefficients
+    """
+    def __init__(self,
+                 orbitals=None,
+                 order=1,  # maximum order of spherical harmonics features
+                 mixing_order=None,
+                 num_features=32,  # dimensionality of the feature space
+                 num_basis_functions=32,  # number of basis functions for featurizing distances
+                 num_radial_components=32,  # number of basis functions for the radial component of the density
+                 # how many modules are stacked for calculating atomic features (iterations)
+                 num_modules=1,
+                 # number of residual blocks applied to atomic features before interaction layer
+                 num_residual_pre_x=1,
+                 # number of residual blocks applied to atomic features after interaction layer
+                 num_residual_post_x=1,
+                 # number of residual blocks applied to atomic features i before computing interaction features
+                 num_residual_pre_vi=1,
+                 # number of residual blocks applied to atomic features j before computing interaction features
+                 num_residual_pre_vj=1,
+                 # number of residual blocks applied to interaction features after combining atomic features i / j
+                 num_residual_post_v=1,
+                 # number of residual blocks applied to atomic features before collecting output atomic features
+                 num_residual_output=1,
+                 # type of radial basis functions (exp - gaussian / exp - bernstein / gaussian / bernstein)
+                 basis_functions='exp-bernstein',
+                 cutoff=15.0,  # cutoff distance (default is 15 Bohr)
+                 # type of activation function used (swish / ssp)
+                 activation='swish',
+                 clebsch_gordan=None,  # instance of the clebsch gordan matrix
+                 calculate_forces=False,
+                 compressed_extraction=False,
+                 verbose=0,
+                 timing=False,
+                 ):  # maximum nuclear charge ( + 1, i.e. 87 for up to Rn) for embeddings, can be kept at default
+        super().__init__()
+
+        # variables to control the flow of the forward graph
+        # (calculate full_hamiltonian / core_hamiltonian / overlap_matrix / energy / forces?)
+
+        self.calculate_forces = calculate_forces
+
+        # store hyperparameter values
+        self.orbitals = orbitals
+        self.order = order
+        self.num_features = num_features
+        self.num_basis_functions = num_basis_functions
+        self.num_modules = num_modules
+        self.num_residual_pre_x = num_residual_pre_x
+        self.num_residual_post_x = num_residual_post_x
+        self.num_residual_pre_vi = num_residual_pre_vi
+        self.num_residual_pre_vj = num_residual_pre_vj
+        self.num_residual_post_v = num_residual_post_v
+        self.num_residual_output = num_residual_output
+        self.num_radial_components = num_radial_components
+        self.basis_functions = basis_functions
+        self.cutoff = cutoff
+        self.activation = activation
+        self.compressed_extraction = compressed_extraction
+        self.verbose = verbose
+        self.timing = timing
+        self.mixing_order = mixing_order
+        if self.mixing_order is None:
+            self.mixing_order = self.order
+
+        print('self order', self.order)
+        if not isinstance(self.order, list):
+            self.order = [self.order] * self.num_modules
+        print('self order', self.order)
+
+        print('self mixing_order', self.mixing_order)
+        if not isinstance(self.mixing_order, list):
+            self.mixing_order = [self.mixing_order] * self.num_modules
+        print('self mixing_order', self.mixing_order)
+
+        if len(self.order) != self.num_modules:
+            print('Order needs to be an integer or a list of integers with length equal to num_modules.' +
+                  ' Taking last order element and using it for all modules.')
+            self.order = [self.order[-1]] * self.num_modules
+
+        if len(self.mixing_order) != self.num_modules:
+            print('Mixing order needs to be an integer or a list of integers with length equal to num_modules.' +
+                  ' Taking last order element and using it for all modules.')
+            self.mixing_order = [self.mixing_order[-1]] * self.num_modules
+
+        N = len(self.orbitals)
+        idx_i = torch.arange(N, dtype=torch.int64).view(-1, 1).repeat(1, N).view(-1)
+        idx_j = torch.arange(N, dtype=torch.int64).view(1, -1).repeat(N, 1).view(-1)
+        # exclude self - interactions
+        idx_i, idx_j = idx_i[idx_i != idx_j], idx_j[idx_i != idx_j]
+        self.register_buffer('idx_i', idx_i)
+        self.register_buffer('idx_j', idx_j)
+
+        self.orbitals_max_order = get_max_order(self.orbitals)
+        self.orbital_spec, _ = combine_orbitals(self.orbitals, self.orbitals_max_order)
+
+        self.order_max = max(self.mixing_order)
+        if clebsch_gordan is None:
+            self.clebsch_gordan = ClebschGordanMatrix()
+        else:
+            self.clebsch_gordan = clebsch_gordan
+        # extract nuclear charges from orbitals, determine maximum order, and
+        # build the occupation mask (for extracting occupied orbitals in energy prediction)
+        if self.basis_functions == 'exp-gaussian':
+            self.radial_basis_functions = ExponentialGaussianRadialBasisFunctions(
+                self.num_basis_functions, self.cutoff)
+        elif self.basis_functions == 'exp-bernstein':
+            self.radial_basis_functions = ExponentialBernsteinRadialBasisFunctions(
+                self.num_basis_functions, self.cutoff)
+        elif self.basis_functions == 'gaussian':
+            self.radial_basis_functions = GaussianRadialBasisFunctions(
+                self.num_basis_functions, self.cutoff)
+        elif self.basis_functions == 'bernstein':
+            self.radial_basis_functions = BernsteinRadialBasisFunctions(
+                self.num_basis_functions, self.cutoff)
+        else:
+            print("basis function type:",
+                  self.basis_functions, "is not supported")
+
+        modules = [ModularBlock(self.order[0], self.num_features, self.num_basis_functions,
+                                self.num_residual_pre_x, self.num_residual_post_x, self.num_residual_pre_vi,
+                                self.num_residual_pre_vj, self.num_residual_post_v, self.num_residual_output,
+                                self.clebsch_gordan, True, self.mixing_order[0], 0, self.activation)]
+        modules.extend([ModularBlock(self.order[i], self.num_features, self.num_basis_functions,
+                                     self.num_residual_pre_x, self.num_residual_post_x, self.num_residual_pre_vi,
+                                     self.num_residual_pre_vj, self.num_residual_post_v, self.num_residual_output,
+                                     self.clebsch_gordan, True, self.mixing_order[i], self.order[i - 1],
+                                     self.activation) for i in range(1, self.num_modules)])
+        self.module = nn.ModuleList(modules)
+
+        self.order_change = [nn.Identity()]
+        for i in range(1, self.num_modules):
+            if self.order[i] != self.order[i - 1]:
+                self.order_change.append(SphericalLinear(self.order[i - 1], self.num_features,
+                                                         self.order[i], self.num_features,
+                                                         clebsch_gordan=self.clebsch_gordan,
+                                                         bias=False))
+            else:
+                self.order_change.append(nn.Identity())
+        self.order_change = nn.ModuleList(self.order_change)
+
+        if self.activation == 'swish':
+            self.out_activation = Swish(self.num_features)
+        elif self.activation == 'ssp':
+            self.out_activation = ShiftedSoftplus(self.num_features)
+        else:
+            print("Unsupported activation function:", self.activation)
+            quit()
+
+        self.energy_output = SphericalLinear(self.order[-1], self.num_features, 0, 1, self.clebsch_gordan)
+
+    def radial_scale_filters(self, L):
+        return getattr(self, "radial_scale_filters_{}".format(L))
+
+    def radial_width_filters(self, L):
+        return getattr(self, "radial_width_filters_{}".format(L))
+
+    def forward(self, atoms):
+        start = time.time()
+        # initialize atomic features to embeddings
+        xs = atoms['sph_repr']
+        dij = atoms['distances']
+        sph = atoms['sph']
+        # print('dij shape', dij.shape)
+        # print('uij shape', uij.shape)
+        # print('R shape', R.shape)
+        rbf = self.radial_basis_functions(dij).unsqueeze_(-2)  # unsqueeze for broadcasting
+
+        # perform iterations over modular building blocks to get environment - dependent features
+        fs = [torch.zeros_like(x) for x in xs]  # output features
+        for i, module in enumerate(self.module):
+            xs = self.order_change[i](xs)
+            xs, ys = module(xs, rbf, sph, self.idx_i, self.idx_j)
+            for L in range(self.order[i] + 1):
+                fs[L] += ys[L]  # add contributions to output features
+        fs[0] = self.out_activation(fs[0])
+
+        atom_en = self.energy_output(fs)[0].squeeze(-1).squeeze(-1)
+
+        energy = torch.sum(atom_en, dim=1, keepdim=True)
+
+        atoms['energy'] = energy
+        if self.calculate_forces:
+            forces = -torch.autograd.grad(torch.sum(energy), atoms['positions'], create_graph=self.training)[0]
+            atoms['forces'] = forces
+
+        if self.timing:
+            print('simple energy time', time.time() - start)
+        return atoms
+
+
+class SimpleRepresentationEnergyNetwork(nn.Module):
+    """
+    Neural network for computing the energy of a molecule based on the density coefficients
+    """
+    def __init__(self,
+                 orbitals=None,
+                 order=1,  # maximum order of spherical harmonics features
+                 mixing_order=None,
+                 num_features=32,  # dimensionality of the feature space
+                 # number of residual blocks applied to atomic features before interaction layer
+                 activation='swish',
+                 clebsch_gordan=None,  # instance of the clebsch gordan matrix
+                 calculate_forces=False,
+                 compressed_extraction=False,
+                 verbose=0,
+                 timing=False,
+                 ):  # maximum nuclear charge ( + 1, i.e. 87 for up to Rn) for embeddings, can be kept at default
+        super().__init__()
+
+        # variables to control the flow of the forward graph
+        # (calculate full_hamiltonian / core_hamiltonian / overlap_matrix / energy / forces?)
+
+        self.calculate_forces = calculate_forces
+
+        # store hyperparameter values
+        self.orbitals = orbitals
+        self.order = order
+        self.num_features = num_features
+        self.activation = activation
+        self.compressed_extraction = compressed_extraction
+        self.verbose = verbose
+        self.timing = timing
+
+        print('self order', self.order)
+        if not isinstance(self.order, list):
+            self.order = [self.order] * self.num_modules
+        print('self order', self.order)
+
+        N = len(self.orbitals)
+        idx_i = torch.arange(N, dtype=torch.int64).view(-1, 1).repeat(1, N).view(-1)
+        idx_j = torch.arange(N, dtype=torch.int64).view(1, -1).repeat(N, 1).view(-1)
+        # exclude self - interactions
+        idx_i, idx_j = idx_i[idx_i != idx_j], idx_j[idx_i != idx_j]
+        self.register_buffer('idx_i', idx_i)
+        self.register_buffer('idx_j', idx_j)
+
+        self.orbitals_max_order = get_max_order(self.orbitals)
+        self.orbital_spec, _ = combine_orbitals(self.orbitals, self.orbitals_max_order)
+
+        self.order_max = max(self.mixing_order)
+        if clebsch_gordan is None:
+            self.clebsch_gordan = ClebschGordanMatrix()
+        else:
+            self.clebsch_gordan = clebsch_gordan
+        if self.activation == 'swish':
+            self.out_activation = Swish(self.num_features)
+        elif self.activation == 'ssp':
+            self.out_activation = ShiftedSoftplus(self.num_features)
+        else:
+            print("Unsupported activation function:", self.activation)
+            quit()
+
+        self.energy_output = SphericalLinear(self.order[-1], self.num_features, 0, 1, self.clebsch_gordan)
+
+    def radial_scale_filters(self, L):
+        return getattr(self, "radial_scale_filters_{}".format(L))
+
+    def radial_width_filters(self, L):
+        return getattr(self, "radial_width_filters_{}".format(L))
+
+    def forward(self, atoms):
+        start = time.time()
+        # initialize atomic features to embeddings
+        xs = atoms['sph_repr']
+
+        # perform iterations over modular building blocks to get environment - dependent features
+        xs = self.out_activation(xs[0])
+
+        atom_en = self.energy_output(xs)[0].squeeze(-1).squeeze(-1)
+
+        energy = torch.sum(atom_en, dim=1, keepdim=True)
+
+        atoms['energy'] = energy
+        if self.calculate_forces:
+            forces = -torch.autograd.grad(torch.sum(energy), atoms['positions'], create_graph=self.training)[0]
+            atoms['forces'] = forces
+
+        if self.timing:
+            print('simple energy time', time.time() - start)
+        return atoms
