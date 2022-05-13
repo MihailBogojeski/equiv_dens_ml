implemented force scaling and fixed some small bugs

src/equiv_dens/data/density_dataset.py
@@ -78,6 +78,7 @@ class AtomsDensityData(Dataset):
         self.verbose = verbose
         self.use_gpu = use_gpu
         self.radii_adjust = radii_adjust
+        self.energy_centered = False
         print('Some variables')
         if required_properties is None:
             self.required_properties = self.available_properties
@@ -204,9 +205,28 @@ class AtomsDensityData(Dataset):
         return idx
 
     def center_energy(self, energy_mean):
-        print('pre centering:', self.atoms['energy'][:20])
-        self.atoms['energy'] -= energy_mean
-        print('post centering:', self.atoms['energy'][:20])
+        if self.verbose > 0:
+            print('energy before centering', np.mean(self.atoms['energy']))
+            print('energy mean', energy_mean)
+        if not self.energy_centered:
+            self.atoms['energy'] -= energy_mean
+            self.energy_centered = True
+        if self.verbose > 0:
+            print('energy after centering', np.mean(self.atoms['energy']))
+
+    @property
+    def energy(self):
+        if self.subset is None:
+            return self.atoms['energy']
+        else:
+            return self.atoms['energy'][self.subset]
+
+    @property
+    def forces(self):
+        if self.subset is None:
+            return self.atoms['forces']
+        else:
+            return self.atoms['forces'][self.subset]
 
     # collects the molecular properties for the batch, should be used as collate_fn
     def get_properties(self, idx):

src/equiv_dens/nn/dft_network.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import numpy as np
 import torch
-from equiv_dens.nn.modules.unit_conversion import UnitConversion
+from equiv_dens.utils.scaling import UnitConversion, VarianceScaling
 
 
 class DFTNetwork(nn.Module):
@@ -14,13 +14,15 @@ class DFTNetwork(nn.Module):
                  calculate_forces_dict=None,
                  verbose=0,
                  conversions_in=UnitConversion(),
-                 conversions_out=UnitConversion()):
+                 conversions_out=UnitConversion(),
+                 scaling=VarianceScaling()):
         super().__init__()
         self.density_repr_model = density_repr_model
         self.property_models = nn.ModuleDict(property_model_dict)
         self.verbose = verbose
         self.conversions_in = conversions_in
         self.conversions_out = conversions_out
+        self.scaling=scaling
         if calculate_forces_dict is None:
             calculate_forces_dict = {key: False for key in property_model_dict.keys()}
             self.calculate_forces = False
@@ -54,8 +56,7 @@ class DFTNetwork(nn.Module):
                 atoms[key] = data[key].clone()
             else:
                 atoms[key] = data[key]
-
-        atoms = self.conversions_in(atoms)
+        
         if self.calculate_forces:
             atoms['positions'].requires_grad = True
 
@@ -90,6 +91,8 @@ class DFTNetwork(nn.Module):
                             print('Memory cached', torch.cuda.memory_cached() / 1024**2)
             # if self.verbose > 2:
             #     print('dft network forward after prop:', key, torch.cuda.memory_summary())
-        atoms = self.conversions_out(atoms)
+        if not self.training:
+            atoms = self.scaling.transform_back(atoms)
+            atoms = self.conversions_out(atoms)
 
         return atoms

src/equiv_dens/training/trainer.py
@@ -343,8 +343,8 @@ class Trainer:
         #     print('param grad', name, param)
 
         data = self._module.conversions_in(data)
+        data = self._module.scaling(data)
         predictions = self._model(data)
-        data = self._module.conversions_out(data)
 
         if self.verbose > -1:
             if 'density' in predictions.keys():
@@ -456,8 +456,10 @@ class Trainer:
             #     print('validate before prediction:', torch.cuda.memory_summary())
             # print('pre-conversion forces:', data['forces'])
             data = self._module.conversions_in(data)
+            data = self._module.scaling(data)
             # print('post-conversion forces:', data['forces'])
             predictions = self._model(data)
+            data = self._module.scaling.transform_back(data)
             data = self._module.conversions_out(data)
             # print('post-post-conversion forces:', data['forces'])
             # if self.verbose > 2: