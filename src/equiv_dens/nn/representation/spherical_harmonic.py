fixed error with std calculation

src/equiv_dens/nn/representation/spherical_harmonic.py
@@ -237,7 +237,9 @@ class EquivariantSphericalHarmonics(nn.Module):
         # initialize atomic features to embeddings
         # repeat Z along batch dimension
         # print('atom numbers shape', atoms['atom_numbers'].shape)
+        # print('atom numbers', atoms['atom_numbers'])
         xs = self.embedding(atoms['atom_numbers'])
+        # print('xs norm representation before:', [float(torch.mean(xs[L]**2)) for L in range(len(xs))])
         mask_dim = neighbor_mask.dim()
         dim_diff = xs[0].dim() - mask_dim
         neighbor_mask = neighbor_mask.to(xs[0]).reshape(rbf.shape[:mask_dim] + (1,) * dim_diff)
@@ -248,10 +250,11 @@ class EquivariantSphericalHarmonics(nn.Module):
             print('Memory allocated', torch.cuda.memory_allocated() / 1024**2)
             print('Memory cached', torch.cuda.memory_cached() / 1024**2)
         fs = [torch.zeros_like(x) for x in xs]  # output features
-        # print('xs norm:', [float(torch.mean(xs[L]**2)) for L in range(len(xs))])
-        # print('fs norm:', [float(torch.mean(fs[L]**2)) for L in range(len(fs))])
+        # print('xs norm representation:', [float(torch.mean(xs[L]**2)) for L in range(len(xs))])
+        # print('fs norm representation:', [float(torch.mean(fs[L]**2)) for L in range(len(fs))])
         for i, module in enumerate(self.module):
             xs = self.order_change[i](xs)
+            # print('xs norm module ', i, ':', [float(torch.mean(xs[L]**2)) for L in range(len(xs))])
             # for L in range(len(xs)):
             #     print('xs[L] main', xs[L])
             xs, ys = module(xs, rbf, sph, idx_i, idx_j, neighbor_mask=neighbor_mask)
@@ -279,6 +282,7 @@ class EquivariantSphericalHarmonics(nn.Module):
         atom_mask = atoms['atom_mask'].reshape(atoms['atom_mask'].shape + (1,) * dim_diff).to(fs[0])
         for i in range(len(fs)):
             fs[i] = fs[i] * atom_mask
+        # print('fs norm representation:', [float(torch.mean(fs[L]**2)) for L in range(len(fs))])
         atoms['sph_repr'] = fs
         if self.timing:
             print('sph repr time', time.time() - start)