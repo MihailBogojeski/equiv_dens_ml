ef439c9e8bd40e5f97ec62212ef81bdd15382895
MihailBogojeski <mihail.bogojeski@campus.tu-berlin.de>
2022-09-02 11:17:56 +0000

    making checkpoint loading work more generally

--- src/equiv_dens/training/model_loader.py
+++ src/equiv_dens/training/model_loader.py
@@ -241,7 +241,7 @@ def load_model(args, dataset, train=False):
                     raise Exception('Unexpected keywords in energy model state dict')
         if len(missing) > 0:
             for key in missing:
-                if args.df_weight > 0:
+                if args.df_weight > 0 and 'property_models.density' not in key:
                     print('Missing keywords', key)
                     raise Exception('Missing keywords in df model state dict')
                 elif args.density_weight > 0 and 'property_models.density' not in key:
