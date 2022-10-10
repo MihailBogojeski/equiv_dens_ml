561763d30a20831bfd7d53a69373d54fd8483052
MihailBogojeski <mihail.bogojeski@campus.tu-berlin.de>
2022-10-10 22:19:45 +0000

    training with dipole moment

--- src/equiv_dens/data/density_dataset.py
+++ src/equiv_dens/data/density_dataset.py
@@ -85,6 +85,13 @@ class AtomsDensityData(Dataset):
         self.projected_density = projected_density
         if required_properties is None:
             self.required_properties = self.available_properties
+        if 'dipole_moment' in self.required_properties:
+            if 'density' not in self.required_properties:
+                self.required_properties.append('density')
+            self.required_properties.remove('dipole_moment')
+            self.calc_dpm = True
+        else:
+            self.calc_dpm = False
         self.centered_positions = center_positions
         self.atoms = np.load(np_path, allow_pickle=True).item()
         if self.atoms['atom_numbers'].ndim == 1:
--- src/equiv_dens/utils/orbitals.py
+++ src/equiv_dens/utils/orbitals.py
@@ -394,3 +394,25 @@ def parse_orbitals(orbitals, atom_types, basis_def):
             split_orbitals.append(orbitals[:, count:(count + (2 * L) + 1)])
             count += (2 * L) + 1
     return split_orbitals
+
+
+def calc_dipole_moment(atoms):
+    density = atoms['density']
+    positive_dens = (density >= 0).to(density)
+    density = density * positive_dens
+    n_electrons = get_n_electrons(atoms['atom_numbers'])
+    scaling_factor = n_electrons / torch.sum(density * atoms['coord_weights'], dim=1, keepdim=True)
+    density = density * scaling_factor
+    positive_dipole_moment = torch.sum(atoms['positions'] * atoms['atom_numbers'].unsqueeze(-1), dim=1)
+    # print('positive_dipole_moment', positive_dipole_moment)
+    weighted_dens = density.unsqueeze(-1) * atoms['coord_weights'].unsqueeze(-1)
+    # print('weighted dens shape', weighted_dens.shape)
+    # print('atom numbers shape', atoms['atom_numbers'].shape)
+    weighted_dens = weighted_dens / torch.sum(weighted_dens, dim=1, keepdim=True) * torch.sum(atoms['atom_numbers'])
+
+    negative_moment = weighted_dens * atoms['coords']
+    negative_dipole_moment = torch.sum(negative_moment, dim=1)
+    # print('negative_dipole_moment', negative_dipole_moment)
+
+    atoms['dipole_moment'] = positive_dipole_moment - negative_dipole_moment
+    return atoms
