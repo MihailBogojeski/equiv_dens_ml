fixed dipole moment calculation

src/equiv_dens/nn/property_output/dipole_moment.py
@@ -12,17 +12,18 @@ class DipoleMomentCalc(nn.Module):
         density = atoms['density']
         positive_dens = (density >= 0).to(density)
         density = density * positive_dens
-        n_electrons = get_n_electrons(atoms['atom_numbers'])
+        n_electrons = get_n_electrons(atoms['batch_atom_numbers'])
         scaling_factor = n_electrons / torch.sum(density * atoms['coord_weights'], dim=1, keepdim=True)
         density = density * scaling_factor
-        positive_dipole_moment = torch.sum(atoms['positions'] * atoms['atom_numbers'].unsqueeze(-1), dim=1)
+        positive_dipole_moment = torch.sum(atoms['batch_positions'] * atoms['batch_atom_numbers'].unsqueeze(-1), dim=1)
         # print('positive_dipole_moment', positive_dipole_moment)
-        weighted_dens = density.unsqueeze(-1) * atoms['coord_weights'].unsqueeze(-1)
-        # print('weighted dens shape', weighted_dens.shape)
-        # print('atom numbers shape', atoms['atom_numbers'].shape)
-        weighted_dens = weighted_dens / torch.sum(weighted_dens, dim=1, keepdim=True) * torch.sum(atoms['atom_numbers'])
+        weighted_dens = density * atoms['coord_weights']
+        # # print('weighted dens shape', weighted_dens.shape)
+        # # print('atom numbers shape', atoms['atom_numbers'].shape)
+        # weighted_dens = weighted_dens / torch.sum(weighted_dens, dim=1, keepdim=True) * \
+        #                 torch.sum(atoms['batch_atom_numbers'], dim=1, keepdim=True)
 
-        negative_moment = weighted_dens * atoms['coords']
+        negative_moment = weighted_dens.unsqueeze(-1) * atoms['coords']
         negative_dipole_moment = torch.sum(negative_moment, dim=1)
         # print('negative_dipole_moment', negative_dipole_moment)
 

src/equiv_dens/test.py
@@ -148,6 +148,7 @@ if args.center_energy:
 loss_weights = {}
 loss_weights['df_coeffs'] = args.df_weight
 loss_weights['density'] = args.density_weight
+loss_weights['dipole_moment'] = args.dipole_moment_weight
 loss_weights['energy'] = args.energy_weight
 loss_weights['forces'] = args.forces_weight
 loss_weights['energy_min'] = args.energy_min_weight
@@ -263,16 +264,12 @@ for test_batch_num, data in enumerate(test_data_loader):
 
     # print('spherical density integral', torch.sum(predictions['density'] * data['coord_weights'], dim=-1))
     # compute error metrics
-    print('predictions keys', predictions.keys())
-    print('data keys', data.keys())
     errors = error_dict.compute(predictions, data)
 
     # update test_errors (running average)
     for key in errors.keys():
         test_errors[key] += (errors[key].item() -
                              test_errors[key]) / (test_batch_num + 1)
-    if 'density' in predictions.keys():
-        print('density shape', predictions['density'].shape)
     predictions = None
     data = None
     errors = None