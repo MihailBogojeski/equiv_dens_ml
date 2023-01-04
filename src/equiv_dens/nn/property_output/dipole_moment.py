9619d5b1840cbc3af559ccc8f83de335974ce76e
Mihail Bogojeski <mihailbogojeski@gmail.com>
2023-01-04 22:02:57 +0100

    more stable dipole moment prediction

--- src/equiv_dens/nn/property_output/dipole_moment.py
+++ src/equiv_dens/nn/property_output/dipole_moment.py
@@ -8,16 +8,20 @@ class DipoleMomentCalc(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, atoms):
+    def forward(self, atoms, center_coordinates=True):
         density = atoms['density']
         positive_dens = (density >= 0).to(density)
         density = density * positive_dens
         n_electrons = get_n_electrons(atoms['batch_atom_numbers'])
         scaling_factor = n_electrons / torch.sum(density * atoms['coord_weights'], dim=1, keepdim=True)
         density = density * scaling_factor
-        # center_of_mass = torch.mean(atoms['batch_positions'] * atoms['batch_atom_numbers'].unsqueeze(-1), dim=1, keepdim=True)
-        # positive_dipole_moment = torch.sum((atoms['batch_positions'] - center_of_mass) * atoms['batch_atom_numbers'].unsqueeze(-1), dim=1)
-        positive_dipole_moment = torch.sum(atoms['batch_positions'] * atoms['batch_atom_numbers'].unsqueeze(-1), dim=1)
+        if center_coordinates:
+            center_of_mass = torch.sum(atoms['batch_positions'] * atoms['batch_atom_numbers'].unsqueeze(-1), dim=1, keepdim=True)\
+                             / torch.sum(atoms['batch_atom_numbers'].unsqueeze(-1) > 0, dim=1, keepdim=True)
+        else:
+            center_of_mass = torch.zeros(atoms['batch_positions'].shape[0], 1, atoms['batch_positions'].shape[-1]).to(atoms['batch_positions'])
+        positive_dipole_moment = torch.sum((atoms['batch_positions'] - center_of_mass) * atoms['batch_atom_numbers'].unsqueeze(-1), dim=1)
+        # positive_dipole_moment = torch.sum(atoms['batch_positions'] * atoms['batch_atom_numbers'].unsqueeze(-1), dim=1)
         # print('positive_dipole_moment', positive_dipole_moment)
         weighted_dens = density * atoms['coord_weights']
         # # print('weighted dens shape', weighted_dens.shape)
@@ -25,8 +29,8 @@ class DipoleMomentCalc(nn.Module):
         # weighted_dens = weighted_dens / torch.sum(weighted_dens, dim=1, keepdim=True) * \
         #                 torch.sum(atoms['batch_atom_numbers'], dim=1, keepdim=True)
 
-        # negative_moment = weighted_dens.unsqueeze(-1) * (atoms['coords'] - center_of_mass.squeeze(1))
-        negative_moment = weighted_dens.unsqueeze(-1) * atoms['coords']
+        negative_moment = weighted_dens.unsqueeze(-1) * (atoms['coords'] - center_of_mass)
+        # negative_moment = weighted_dens.unsqueeze(-1) * atoms['coords']
         negative_dipole_moment = torch.sum(negative_moment, dim=1)
         # print('negative_dipole_moment', negative_dipole_moment)
 
