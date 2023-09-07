d7be67793364ba8bef32516d323295abf0caaa4d
Mihail Bogojeski <mihailbogojeski@gmail.com>
2023-09-07 19:26:01 +0200

    Fixing consistency issues in testing scripts

--- src/equiv_dens/train_all.py
+++ src/equiv_dens/train_all.py
@@ -371,6 +371,9 @@ for phase in training_phases:
     loss_comp_weights['df_coeffs'] = {loss_comp: loss_weight
                                       for loss_comp, loss_weight
                                       in zip(args.df_loss_comp, args.df_loss_comp_weights)}
+    loss_comp_weights['dipole_moment'] = {loss_comp: loss_weight
+                                          for loss_comp, loss_weight
+                                          in zip(args.dipole_moment_loss_comp, args.dipole_moment_loss_comp_weights)}
     loss_comp_weights['energy'] = {loss_comp: loss_weight
                                    for loss_comp, loss_weight
                                    in zip(args.energy_loss_comp, args.energy_loss_comp_weights)}
@@ -706,6 +709,7 @@ loss_comp['dipole_moment'] = args.dipole_moment_loss_comp
 loss_comp['df_coeffs'] = args.df_loss_comp
 loss_comp['energy'] = args.energy_loss_comp
 loss_comp['forces'] = args.forces_loss_comp
+loss_comp['dipole_moment'] = args.dipole_moment_loss_comp
 
 loss_comp_weights = {}
 loss_comp_weights['density'] = {'perc_mae': 1, 'perc_rmse': 1}
@@ -718,6 +722,9 @@ loss_comp_weights['energy'] = {loss_comp: loss_weight
 loss_comp_weights['forces'] = {loss_comp: loss_weight
                                for loss_comp, loss_weight
                                in zip(args.forces_loss_comp, args.forces_loss_comp_weights)}
+loss_comp_weights['dipole_moment'] = {loss_comp: loss_weight
+                                      for loss_comp, loss_weight
+                                      in zip(args.dipole_moment_loss_comp, args.dipole_moment_loss_comp_weights)}
 
 
 error_dict = ErrorDict(loss_weights, weights_balance=args.weights_balance,
@@ -757,8 +764,11 @@ for test_batch_num, data in enumerate(test_data_loader):
 
     # update test_errors (running average)
     for key in errors.keys():
-        test_errors[key] += (errors[key].item() -
-                             test_errors[key]) / (test_batch_num + 1)
+        if key not in test_errors.keys():
+            test_errors[key] = errors[key].item()
+        else:
+            test_errors[key] += (errors[key].item() -
+                                 test_errors[key]) / (test_batch_num + 1)
     predictions = None
     data = None
     errors = None
