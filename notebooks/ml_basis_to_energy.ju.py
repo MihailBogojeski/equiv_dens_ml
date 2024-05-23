still working on correct direct gradient, checkpoint

notebooks/ml_basis_to_energy.ju.py
@@ -721,7 +721,8 @@ for i in range(res['batch_positions'].shape[1]):
     # rho_atoms += rho_at
 # %%
 # Load dataset with option for density gradients
-args.spherical_grid_level = 4
+args.spherical_grid_level = 1
+args.density_grad = False
 grid_fn = partial(spherical_grid, level=args.spherical_grid_level)
 dataset = AtomsDensityData(np_path=args.np_dataset_test, density_path=args.dens_dataset_test,
                            orbitals_path=args.orbitals_file,
@@ -755,21 +756,43 @@ print('num samples', main_args.num_samples)
 print('args use gpu', args.use_gpu)
 
 # %%
+model = model_loader.load_model(args, dataset)
+model.eval()
+idx = [3]
+samp = dataset.get_properties(idx)
+samp_df = dataset_df.get_properties(idx)
+
+res = model(samp)
+
+print('samp df_coeffs', samp_df['df_coeffs'].shape)
+print('res df_coeffs', res['df_coeffs'].shape)
+
+# print('res_radial width', res['radial_width'])
+# print('dataset radial coeffs', dataset.radial_coeffs)
+print('res density integral', torch.sum(res['density'] * res['coord_weights'], dim=1))
+print('density integral', torch.sum(samp['density'] * samp['coord_weights'], dim=1))
+print('density integral', torch.sum(samp_df['density'] * samp_df['coord_weights'], dim=1))
+print('density error', torch.sum(torch.abs(res['density'] - samp['density']) * samp['coord_weights'], dim=1)
+                       / torch.sum(samp['batch_atom_numbers'], dim=1))
+print('density df error', torch.sum(torch.abs(samp_df['density'] - samp['density']) * samp['coord_weights'], dim=1)
+                          / torch.sum(samp['batch_atom_numbers'], dim=1))
+print('res density grad', res['density_grad'].shape)
+# %%
 # Evaluate PBE energy for density with derivatives directly from data loader
 ni = numint.NumInt()
 samp = dataset.get_properties(idx)
 coords_scale = (samp['coords'] - samp['pos_shift']) / param.BOHR
 print('dens integral', torch.sum(samp['density'] * samp['coord_weights']))
 dens_pbe = torch.permute(torch.cat([samp['density'].unsqueeze(-1), samp['density_grad']], dim=-1), (2, 1, 0)).squeeze()
+print('dens_deriv integral', torch.sum(torch.abs(dens_pbe[1:] * samp['coord_weights'])))
 print(dens_pbe.shape)
 exc_eff_pbe = torch.from_numpy(ni.eval_xc_eff('pbe', dens_pbe.numpy(force=True).astype(np.double), deriv=1, xctype='GGA')[0])
 dens_calc = dens_pbe[0] * samp['coord_weights']
 # print('exchange correlation', exc_eff_pbe.shape)
-exc_sum_pbe = torch.sum(exc_eff_pbe * dens_calc)
-print('exc_pbe', exc_pbe)
-print('exc diff', np.abs(exc_sum_pbe - exc_pbe))
-print('exc diff kcal/mol', utils.hartree_to_kcal(np.abs(exc_sum_pbe - exc_pbe)))
-
+exc_pbe = torch.sum(exc_eff_pbe * dens_calc)
+# print('exc_pbe', exc_pbe)
+# print('exc diff', np.abs(exc_sum_pbe - exc_pbe))
+# print('exc diff kcal/mol', utils.hartree_to_kcal(np.abs(exc_sum_pbe - exc_pbe)))
 # %%
 # Evaluate PBE energy for ML coeffs coreless, from spline from dataset
 auxmol_pbe = orbitals.ml_basis_to_auxmol(res)
@@ -781,6 +804,10 @@ ao = numint.eval_ao(auxmol_pbe, coords_scale[0], deriv=1)
 rho_pbe = np.einsum('ijk,k->ij', ao, df_coeffs_pbe)
 dens_pbe = torch.from_numpy(rho_pbe)
 print('dens integral', torch.sum(dens_pbe[[0]] * samp['coord_weights']))
+print('dens_deriv integral', torch.sum(torch.abs(dens_pbe[1:] * samp['coord_weights'])))
+print('dens pbd 1: shape', dens_pbe[1:].shape)
+print('atom density grad shape', samp['atom_density_grad'][0].shape)
+print('atom dens_deriv integral', torch.sum(torch.abs(samp['atom_density_grad'][0].t() * samp['coord_weights'])))
 print('dens_pbe shape', dens_pbe.shape)
 print('atom dens dataset shape', samp['atom_density'].shape)
 print('atom dens grad dataset shape', samp['atom_density_grad'].shape)
@@ -797,6 +824,21 @@ ni = numint.NumInt()
 exc_eff_pbe = torch.from_numpy(ni.eval_xc_eff('pbe', rho_pbe, deriv=1, xctype='GGA')[0])
 dens_calc = dens_pbe[0] * samp['coord_weights']
 print('exchange correlation', exc_eff_pbe.shape)
+exc_sum_pbe = torch.sum(exc_eff_pbe * dens_calc)
+print('exc energy', exc_sum_pbe)
+print('exc diff', np.abs(exc_sum_pbe - exc_pbe))
+print('exc diff kcal/mol', utils.hartree_to_kcal(np.abs(exc_sum_pbe - exc_pbe)))
+# %%
+# Evaluate PBE energy for ML coeffs coreless, from spline from dataset
+coords_scale = (samp['coords'] - samp['pos_shift']) / param.BOHR
+res = model(samp)
+dens_pbe = torch.permute(torch.cat([res['density'].unsqueeze(-1), res['density_grad']], dim=-1), (2, 1, 0)).squeeze()
+print(dens_pbe.shape)
+print('dens integral', torch.sum(dens_pbe[[0]] * samp['coord_weights']))
+print('dens_deriv integral', torch.sum(torch.abs(dens_pbe[1:] * samp['coord_weights'])))
+exc_eff_pbe = torch.from_numpy(ni.eval_xc_eff('pbe', dens_pbe.numpy(force=True).astype(np.double), deriv=1, xctype='GGA')[0])
+dens_calc = dens_pbe[0] * samp['coord_weights']
+# print('exchange correlation', exc_eff_pbe.shape)
 exc_sum_pbe = torch.sum(exc_eff_pbe * dens_calc)
 print('exc energy', exc_sum_pbe)
 print('exc diff', np.abs(exc_sum_pbe - exc_pbe))

src/equiv_dens/nn/property_output/density.py
@@ -7,7 +7,7 @@ from equiv_dens.utils.spherical_harmonics import spherical_harmonics
 import equiv_dens.utils.spherical_harmonics_deriv as sph_deriv
 from equiv_dens.utils.orbitals import combine_orbital_basis, \
     gaussian_rbf, get_max_order, get_n_electrons, coeffs_dict_to_vector, \
-    vector_to_coeffs_dict, gto_norm, pyscf_gto_factor
+    vector_to_coeffs_dict, gto_norm, pyscf_gto_factor, eval_gto, eval_gto_grad
 from equiv_dens.utils.base import calculate_distances_and_directions, batch_compressed_atoms
 import numpy as np
 import time
@@ -41,7 +41,7 @@ class CoeffsIntegralConstraint(nn.Module):
                     L0_width.append(width)
         L0_coeffs_comb = torch.cat([coeff.view((coeff.shape[0], -1)) for coeff in L0_coeffs], dim=1)
         L0_widths_comb = torch.cat([width.view((width.shape[0], -1)) for width in L0_width], dim=1)
-        norms = 1/gto_norm(0, L0_widths_comb)
+        norms = 1 / gto_norm(0, L0_widths_comb)
         coeffs_sum = torch.sum(L0_coeffs_comb * norms / pyscf_gto_factor, dim=1, keepdim=True)
         scale_factor = n_electrons / coeffs_sum
         scale_factor = scale_factor.reshape(*scale_factor.shape, 1, 1)
@@ -702,9 +702,6 @@ class DensityExpansion(nn.Module):
     def __init__(self, orbital_basis,
                  expansion_constraint=None,
                  integral_constraint=None,
-                 softmax_norm=False,
-                 n_electrons=None,
-                 integral_scale=False,
                  verbose=0,
                  timing=False,
                  memory=False,
@@ -716,7 +713,6 @@ class DensityExpansion(nn.Module):
         self.orbital_basis = orbital_basis
         self.expansion_constraint = expansion_constraint
         self.integral_constraint = integral_constraint
-        self.softmax_norm = softmax_norm
         self.timing = timing
         self.memory = memory
         self.verbose = verbose
@@ -726,10 +722,7 @@ class DensityExpansion(nn.Module):
             self.register_buffer('grid_scaling_factor', torch.ones(size=(1,)))
         else:
             self.grid_scaling_factor = 0
-        if integral_scale:
-            self.register_parameter('integral_scale', nn.Parameter(torch.ones(size=(1,))))
-        else:
-            self.register_buffer('integral_scale', torch.ones(size=(1,)))
+        # self.register_buffer('integral_scale', torch.ones(size=(1,)))
         if self.verbose:
             print('expansion constraint', self.expansion_constraint)
             print('integral constraint', self.integral_constraint)
@@ -766,11 +759,8 @@ class DensityExpansion(nn.Module):
         if eval_L is None:
             eval_L = list(range(self.orbitals_max_order + 1))
         atoms['density'] = 0
-        L0_coeffs = []
-        L0_sph = []
-        L0_d = []
-        L0_i = []
-        L0_width = []
+        if density_grad:
+            atoms['density_grad'] = 0
         n_electrons = get_n_electrons(atoms['batch_atom_numbers'])
         for i in range(n_eval):
             if self.verbose > 1 and self.memory:
@@ -786,7 +776,6 @@ class DensityExpansion(nn.Module):
             s = spherical_harmonics(self.orbitals_max_order_dict[z], u)
             if density_grad:
                 s_deriv = sph_deriv.spherical_harmonics_deriv(self.orbitals_max_order_dict[z], u)
-                dcoords = torch.eye(3).unsqueeze(0).unsqueeze(0)
             for L in range(len(s)):
                 zeros = torch.zeros_like(s[L])
                 s[L] = torch.where(torch.isnan(s[L]), zeros, s[L])  # making sure there are no nans to avoid NaNs
@@ -811,82 +800,20 @@ class DensityExpansion(nn.Module):
                     scale = scale.reshape(scale.shape[:2] +
                                           (1,) * (dim_diff + 1) +
                                           scale.shape[2:])
-                if L == 0:
-                    L0_coeff = sph_coeff * scale
-                    L0_coeffs.append(L0_coeff)
-                    L0_sph.append(s[L])
-                    L0_d.append(d)
-                    L0_i.append(i)
-                    L0_width.append(width)
-                    continue
                 if i in eval_atoms and L in eval_L:
-                    sph = s[L].unsqueeze(-1) * sph_coeff
-                    if density_grad:
-                        if L != 1:
-                            sph_grad = (s_deriv[L].unsqueeze(-1) * sph_coeff.unsqueeze(0)).sum((-1, -2))
-                        else:
-                            sph_grad = (s_deriv[L].unsqueeze(-1) * sph_coeff[..., [2, 0, 1], :]).sum(-1)
-                    rbf = gaussian_rbf(d.unsqueeze(-1), width, scale, L)
+                    density_part, sph, rbf = eval_gto(s[L], d, L, sph_coeff, width, scale)
                     if self.verbose > 2:
                         print('L', L)
                         print('rbf integral', torch.sum(rbf * atoms['coord_weights'].unsqueeze(-1).unsqueeze(-1), dim=(-2, -3)))
                         print('abs sph', torch.sum(torch.abs(sph) * atoms['coord_weights'].unsqueeze(-1).unsqueeze(-1), dim=(-2, -3)))
-                    atoms['density'] += torch.sum(rbf * sph, dim=(-2, -1))
+                    atoms['density'] += density_part
+                    if density_grad:
+                        density_part_grad = eval_gto_grad(s_deriv[L], d, L, atoms['coords'] - pos,
+                                                          sph_coeff, width, scale, sph, rbf)
+                        atoms['density_grad'] += density_part_grad
+
         if self.verbose > 0:
             print('Density shape', atoms['density'].shape)
-        L0_coeffs_comb = torch.cat([coeff.view((coeff.shape[0], -1)) for coeff in L0_coeffs], dim=1)
-        L0_widths_comb = torch.cat([width.view((width.shape[0], -1)) for width in L0_width], dim=1)
-        atoms['L0_coeffs'] = L0_coeffs_comb
-        atoms['L0_widths'] = L0_coeffs_comb
-        if self.integral_constraint is True or self.integral_constraint == 'coeffs':
-            if self.softmax_norm:
-                L0_coeffs_comb = F.softmax(L0_coeffs_comb, dim=1)
-                L0_coeffs_comb = L0_coeffs_comb * n_electrons
-                L0_coeffs_comb = L0_coeffs_comb * torch.clamp(self.integral_scale, 0.5, 1.5)
-            else:
-                norms = 1/gto_norm(0, L0_widths_comb)
-                coeffs_sum = torch.sum(L0_coeffs_comb * norms / pyscf_gto_factor, dim=1, keepdim=True)
-                if self.remove_atom_density:
-                    L0_coeffs_abs = torch.abs(L0_coeffs_comb)
-                    L0_coeffs_abs = torch.sign(coeffs_sum) * L0_coeffs_abs
-                    L0_coeffs_abs_sum = torch.sum(L0_coeffs_abs * norms / pyscf_gto_factor, dim=1, keepdim=True)
-                    scale_offset = (L0_coeffs_abs / (L0_coeffs_abs_sum + 1e-10)) * coeffs_sum
-                    L0_coeffs_comb = L0_coeffs_comb - scale_offset
-                else:
-                    scale_factor = n_electrons / coeffs_sum
-                    L0_coeffs_comb = L0_coeffs_comb * scale_factor
-                    L0_coeffs_comb = L0_coeffs_comb * torch.clamp(self.integral_scale, 0.5, 1.5)
-        coeffs_pointer = 0
-        L0_integrals = []
-        if 0 in eval_L:
-            for i in range(len(L0_coeffs)):
-                coeffs_size = np.prod(list(L0_coeffs[i].shape[1:]))
-                curr_coeffs = L0_coeffs_comb[:, coeffs_pointer:(coeffs_size + coeffs_pointer)]
-                if self.verbose > 3:
-                    print('curr_coeffs', curr_coeffs)
-                    print('curr_coeffs sum', torch.sum(curr_coeffs))
-                    print('L0 width', L0_width[i])
-                curr_coeffs = curr_coeffs.view(L0_coeffs[i].shape)
-                coeffs_pointer += coeffs_size
-                normalize = False
-                if (self.integral_constraint is True or self.integral_constraint == 'coeffs') and self.softmax_norm:
-                    normalize = True
-                rbf = gaussian_rbf(L0_d[i].unsqueeze(-1), L0_width[i], curr_coeffs, 0, normalize=normalize)
-                sph = L0_sph[i].unsqueeze(-1)
-                if self.verbose > 2:
-                    print('L', 0)
-                    print('rbf integral', torch.sum(rbf * atoms['coord_weights'].unsqueeze(-1).unsqueeze(-1), dim=(-2, -3)))
-                    print('abs sph', torch.sum(torch.abs(sph) * atoms['coord_weights'].unsqueeze(-1).unsqueeze(-1), dim=(-2, -3)))
-                if L0_i[i] in eval_atoms:
-                    L0_dens = torch.sum(rbf * sph, dim=(-1, -2))
-                    L0_int = torch.sum(L0_dens * atoms['coord_weights'], dim=-1)
-                    L0_integrals.append(L0_int)
-                    if self.verbose > 2:
-                        print('L0_dens integral', L0_int)
-                        print('l0 dens shape', L0_dens.shape)
-                        print('atoms density shape', atoms['density'].shape)
-                    atoms['density'] += L0_dens
-        if self.verbose > 0:
             print('L0_int sum', torch.sum(torch.cat(L0_integrals)))
             print('sum neg integrals', torch.sum((atoms['density'] * atoms['coord_weights'])[atoms['density'] < 0], dim=-1))
         if self.expansion_constraint == 'sq':

src/equiv_dens/training/utils.py
@@ -67,6 +67,8 @@ def init_training_vars(args, hyperparam_args):
         step = checkpoint['step']
         restore = True
         data_split_indices = checkpoint['data_split_indices']
+    if args.density_grad_weight > 0:
+        args.density_grad = True
     print('args core density basis', args.core_density_basis)
     train_vars = {'model_code': model_code, 'directory': directory, 'checkpoint': checkpoint,
                   'step': step, 'restore': restore, 'data_split_indices': data_split_indices}
@@ -235,7 +237,11 @@ def prepare_cubic_datasets(args, required_properties, train_indices, valid_indic
                                         sampling_fn=cube_sampling_fn,
                                         verbose=args.verbose,
                                         cutoff=args.cutoff,
-                                        df_loss_weights=args.df_loss_weights)
+                                        df_loss_weights=args.df_loss_weights,
+                                        atom_dens_path=args.atom_dens_path,
+                                        atom_dens_type=args.atom_dens_type,
+                                        density_grad=args.density_grad,
+                                        )
 
         valid_cube_dataset = torch.utils.data.Subset(cube_dataset, valid_indices)
 
@@ -291,6 +297,7 @@ def prepare_datasets(args, required_properties, grid_vars, data_split_indices, d
                                df_loss_weights=args.df_loss_weights,
                                atom_dens_path=args.atom_dens_path,
                                atom_dens_type=args.atom_dens_type,
+                               density_grad=args.density_grad,
                                )
 
 # split into train / valid / test
@@ -323,6 +330,7 @@ def prepare_datasets(args, required_properties, grid_vars, data_split_indices, d
                                          df_loss_weights=args.df_loss_weights,
                                          atom_dens_path=args.atom_dens_path,
                                          atom_dens_type=args.atom_dens_type,
+                                         density_grad=args.density_grad,
                                          )
 
         if data_split_indices is None or args.ignore_split_indices:
@@ -365,6 +373,7 @@ def prepare_datasets(args, required_properties, grid_vars, data_split_indices, d
                                         df_loss_weights=args.df_loss_weights,
                                         atom_dens_path=args.atom_dens_path,
                                         atom_dens_type=args.atom_dens_type,
+                                        density_grad=args.density_grad,
                                         )
 
         if args.num_test is not None:
@@ -420,6 +429,7 @@ def init_error_dict(args, test=False):
     """
     loss_weights = {}
     loss_weights['density'] = args.density_weight
+    loss_weights['density_grad'] = args.density_grad_weight
     loss_weights['dipole_moment'] = args.dipole_moment_weight
     loss_weights['df_coeffs'] = args.df_weight
     loss_weights['energy'] = args.energy_weight
@@ -453,18 +463,24 @@ def init_error_dict(args, test=False):
                                       in zip(args.df_loss_comp, args.df_loss_comp_weights)}
     loss_comp_weights['dipole_moment'] = {loss_comp: loss_weight
                                           for loss_comp, loss_weight
-                                          in zip(args.dipole_moment_loss_comp, args.dipole_moment_loss_comp_weights)}
+                                          in zip(args.dipole_moment_loss_comp,
+                                                 args.dipole_moment_loss_comp_weights)}
     loss_comp_weights['energy'] = {loss_comp: loss_weight
                                    for loss_comp, loss_weight
                                    in zip(args.energy_loss_comp, args.energy_loss_comp_weights)}
     loss_comp_weights['forces'] = {loss_comp: loss_weight
                                    for loss_comp, loss_weight
                                    in zip(args.forces_loss_comp, args.forces_loss_comp_weights)}
+    loss_comp_weights['density_grad'] = {loss_comp: loss_weight
+                                         for loss_comp, loss_weight
+                                         in zip(args.density_grad_loss_comp,
+                                                args.density_grad_loss_comp_weights)}
     if not test:
         loss_comp['density'] = args.density_loss_comp
         loss_comp_weights['density'] = {loss_comp: loss_weight
                                         for loss_comp, loss_weight
-                                        in zip(args.density_loss_comp, args.density_loss_comp_weights)}
+                                        in zip(args.density_loss_comp,
+                                               args.density_loss_comp_weights)}
         error_dict = ErrorDict(loss_weights, weights_balance=args.weights_balance,
                                percentage_error=args.percentage_error,
                                weights_decay=weights_decay, weights_min=weights_min,

src/equiv_dens/utils/orbitals.py
@@ -129,6 +129,7 @@ def gaussian_rbf_deriv(r, width, scale, order, normalize=False):
     # print('width', width.shape)
     # print('r shape', r.shape)
     # print('L', order)
+
     if normalize:
         # scale_calc = scale * (width**(3 / 2)) / (np.pi**(3 / 2)) * utils.to_angstrom**3
         # scale_calc = scale * (width**(3/2)) / (np.pi**(3 / 2))
@@ -139,7 +140,6 @@ def gaussian_rbf_deriv(r, width, scale, order, normalize=False):
     # print('width', width)
     # print('gto_norm', 1/gto_norm(order, width))
     # print('pyscf gto factor', pyscf_gto_factor)
-    # print('scale calc', scale_calc)
     # print('scale calc * gto norm', scale_calc/gto_norm(order, width))
     r_bohr = r * utils.to_bohr
     rbf_deriv = (
@@ -149,7 +149,6 @@ def gaussian_rbf_deriv(r, width, scale, order, normalize=False):
         * torch.exp(-width * (r_bohr) ** 2)
     )
     rbf_deriv *= utils.to_bohr
-    # print('rbf_deriv internal shape', rbf_deriv.shape)
     # rbf = scale_calc * torch.exp(-width * (r_bohr)**2)
     return torch.sum(rbf_deriv, dim=-2, keepdim=True)
 
@@ -1158,7 +1157,7 @@ def sample_atom_density(
     if individual_dens:
         atom_densities = torch.stack(atom_densities, dim=1)
     else:
-        atom_densities = torch.empty()
+        atom_densities = torch.zeros(1)
     return dens, atom_densities
 
 
@@ -1271,3 +1270,69 @@ def model_input_from_atoms(
     inputs["positions"] = inputs["positions"][:, inputs["atom_mask"]]
 
     return inputs
+
+
+def eval_gto(s, d, L, sph_coeff, width, scale):
+    """
+    Function to evaluate GTO from the spherical harmonics and basis coefficients.
+
+    Args:
+        s: spherical harmonics evaluation for a set of gridpoint
+        d: distances to each of the gridpoints
+        L: degree of spherical harmonics
+        sph_coeff: coefficients for angular part of GTO
+        width: width coefficient for radial part of GTO
+        scale: scale coefficient for radial part of GTO
+    """
+    sph = s.unsqueeze(-1) * sph_coeff
+    rbf = gaussian_rbf(d.unsqueeze(-1), width, scale, L)
+    return torch.sum(rbf * sph, dim=(-2, -1)), sph, rbf
+
+
+def eval_gto_grad(s_deriv, d, L, coords, sph_coeff, width, scale, sph, rbf):
+    """
+    Function to evaluate GTO gradient given spherical harmonic gradients and basis coefficients.
+
+    Args:
+        s_deriv: derivative of spherical harmonics for a set of gridpoint
+        d: distances to each of the gridpoints
+        u: direction vectors to each of the gridpoints
+        L: degree of spherical harmonics
+        sph_coeff: coefficients for angular part of GTO
+        width: width coefficient for radial part of GTO
+        scale: scale coefficient for radial part of GTO
+    """
+    print('s deriv shape', s_deriv.shape)
+    print('d shape', d.shape)
+    print('coords shape', coords.shape)
+    print('sph coeff shape', sph_coeff.shape)
+    print('width shape', width.shape)
+    print('scale shape', scale.shape)
+    print('sph shape', sph.shape)
+    print('rbf shape', rbf.shape)
+    dcoords = torch.eye(3).unsqueeze(0).unsqueeze(0)
+    # gradient of spherical harmonics expansion with respect to u
+    sph_grad = (s_deriv.unsqueeze(-1) * sph_coeff.unsqueeze(-2)).sum((-1, -3))
+    # gradient of spherical harmonics expansion with respect to grid coordinates
+    sph_grad_c = sph_grad.unsqueeze(-2) * -(dcoords / d.unsqueeze(-1)
+                                            - ((coords).unsqueeze(-2)
+                                            * (coords).unsqueeze(-1)
+                                            / d.unsqueeze(-1)**3))
+    sph_grad_c = sph_grad_c.sum(-1)
+    zeros = torch.zeros_like(sph_grad_c)
+    sph_grad_c = torch.where(torch.isnan(sph_grad_c), zeros, sph_grad_c)  # making sure there are no nans to avoid NaNs
+
+    print('gaussian rbd grad shape, ', gaussian_rbf_deriv(d.unsqueeze(-1), width, scale, L).shape)
+    print('coords/d shape', (coords / d).unsqueeze(-1).shape)
+    # gradient of gaussian radial functions with respect to coordinates
+    rbf_grad = gaussian_rbf_deriv(d.unsqueeze(-1), width, scale, L) * (coords / d).unsqueeze(-1)
+    zeros = torch.zeros_like(rbf_grad)
+    rbf_grad = torch.where(torch.isnan(rbf_grad), zeros, rbf_grad)  # making sure there are no nans to avoid NaNs
+    print('rbf grad', rbf_grad.shape)
+    print('sph_grad_c', sph_grad_c.shape)
+    # gradient of gto basis with respect to coordinates
+    gto_deriv = torch.sum(rbf_grad.unsqueeze(-2) * sph.unsqueeze(-3), -2) + rbf * sph_grad_c.unsqueeze(-1)
+    gto_deriv = gto_deriv.sum(-1)
+    print('gto_deriv.shape', gto_deriv.shape)
+    print('')
+    return gto_deriv