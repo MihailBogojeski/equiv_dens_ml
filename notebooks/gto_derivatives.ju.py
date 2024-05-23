still working on correct direct gradient, checkpoint

notebooks/gto_derivatives.ju.py
@@ -15,7 +15,9 @@ coords = torch.randn(1, 3, 3)
 coords.requires_grad = True
 print('coords', coords)
 max_L = 5
-d, u = utils.calculate_distances_and_directions(coords, center=torch.zeros(1, 1, 3))
+center = torch.ones(1, 1, 3) / 10
+coords = torch.cat([coords, center], dim=1)
+d, u = utils.calculate_distances_and_directions(coords, center=center)
 s = spherical_harmonics(max_L, u)
 print('s requires grad', [ss.requires_grad for ss in s])
 for L in range(len(s)):
@@ -23,9 +25,9 @@ for L in range(len(s)):
     s[L] = torch.where(torch.isnan(s[L]), zeros, s[L])  # making sure there are no nans to avoid NaNs
 print('dist', d)
 print('unit', u)
-scales = [torch.randn(1, 1, 1, 1) for L in range(max_L + 1)]
-widths = [torch.randn(1, 1, 1, 1)**2 for L in range(max_L + 1)]
-coeffs = [torch.randn(1, 1, 2 * L + 1, 1) for L in range(max_L + 1)]
+scales = [torch.randn(1, 1, 1, 2) for L in range(max_L + 1)]
+widths = [torch.randn(1, 1, 1, 2)**2 for L in range(max_L + 1)]
+coeffs = [torch.randn(1, 1, 2 * L + 1, 2) for L in range(max_L + 1)]
 print([ss.shape for ss in s])
 print('scales', scales)
 print('widths', widths)
@@ -65,66 +67,93 @@ print('u', u)
 gto = 0
 gto_grad = 0
 s_deriv = sph_deriv.spherical_harmonics_deriv(max_L, u)
+# for L in range(len(s_deriv)):
+#     zeros = torch.zeros_like(s_deriv[L])
+#     s_deriv[L] = torch.where(torch.isnan(s_deriv[L]), zeros, s_deriv[L])  # making sure there are no nans to avoid NaNs
 dcoords = torch.eye(3).unsqueeze(0).unsqueeze(0)
 print(dcoords.shape)
+# TODO: check robustness for zero coords
 for L in range(max_L + 1):
     scale = scales[L]
     width = widths[L]
     coeff = coeffs[L]
     sph = s[L].unsqueeze(-1) * coeff
     print('L', L)
-    print('coeff shape', coeff.shape)
     print('s[L] shape', s[L].shape)
     print('s[l] deriv shape', s_deriv[L].shape)
-    print('sph', sph)
+    print('d shape', d.shape)
+    print('coords shape', coords.shape)
+    print('sph coeff shape', coeff.shape)
+    print('width shape', width.shape)
+    print('scale shape', scale.shape)
+    print('sph shape', sph.shape)
+    print('rbf shape', rbf.shape)
     sph_autograd = 0
     du = 0
     if L != 0:
-        for i in range(sph.squeeze().shape[0]):
-            for j in range(sph.squeeze().shape[1]):
-                sph_autograd += torch.autograd.grad(sph.squeeze()[i, j], coords, retain_graph=True)[0]
-                du += torch.autograd.grad(sph.squeeze()[i, j], u, retain_graph=True)[0]
+        for i in range(sph.squeeze(0).shape[0]):
+            for j in range(sph.squeeze(0).shape[1]):
+                for k in range(sph.squeeze(0).shape[2]):
+                    sph_autograd += torch.autograd.grad(sph.squeeze(0)[i, j, k], coords, retain_graph=True)[0]
+                    du += torch.autograd.grad(sph.squeeze(0)[i, j, k], u, retain_graph=True)[0]
                 # print(sph_autograd)
     # print('sph du autograd', du)
-    if L != 1:
-        sph_grad = (s_deriv[L].unsqueeze(-1) * coeff.unsqueeze(0)).sum((-1, -2))
-    else:
-        sph_grad = (s_deriv[L].unsqueeze(-1) * coeff[..., [2, 0, 1], :]).sum(-1)
-    # print('sph du deriv', sph_grad)
-    # print('sph autograd', sph_autograd)
+    print('s deriv cooeffs size', (s_deriv[L].unsqueeze(-1) * coeff.unsqueeze(-2)).shape)
+    sph_grad = (s_deriv[L].unsqueeze(-1) * coeff.unsqueeze(-2)).sum((-1, -3))
     sph_grad_c = sph_grad.unsqueeze(-2) * -(dcoords/d.unsqueeze(-1) -
-                 (coords.unsqueeze(-2) * coords.unsqueeze(-1)/d.unsqueeze(-1)**3))
+                 ((coords - center).unsqueeze(-2) *(coords - center).unsqueeze(-1)/d.unsqueeze(-1)**3))
     sph_grad_c = sph_grad_c.sum(-1)
-    # print('sph u and c ratio', sph_autograd / sph_grad)
+    zeros = torch.zeros_like(sph_grad_c)
+    sph_grad_c = torch.where(torch.isnan(sph_grad_c), zeros, sph_grad_c)  # making sure there are no nans to avoid NaNs
+    # print('sph_grad_c', sph_grad_c)
+    # print('sph_autograd', sph_autograd)
     rbf = orbitals.gaussian_rbf(d.unsqueeze(-1), width, scale, L)
     rbf_autograd = 0
-    for i in range(3):
-        rbf_autograd += torch.autograd.grad(rbf.squeeze()[i], coords, retain_graph=True)[0]
+    for i in range(rbf.squeeze(0, -2).shape[0]):
+        for j in range(rbf.squeeze(0, -2).shape[1]):
+            rbf_autograd += torch.autograd.grad(rbf.squeeze(0, -2)[i, j], coords, retain_graph=True)[0]
     # print('rbf autograd', rbf_autograd)
-    rbf_grad = orbitals.gaussian_rbf_deriv(d.unsqueeze(-1), width, scale, L).squeeze(-1) * coords / d
+    print('gaussian rbd grad shape, ', orbitals.gaussian_rbf_deriv(d.unsqueeze(-1), width, scale, L).shape)
+    print('coords/d shape', ((coords - center) / d).unsqueeze(-1).shape)
+    rbf_grad = orbitals.gaussian_rbf_deriv(d.unsqueeze(-1), width, scale, L) * ((coords - center) / d).unsqueeze(-1)
+    print('rbf_grad.shape', rbf_grad.shape)
+    zeros = torch.zeros_like(rbf_grad)
+    rbf_grad = torch.where(torch.isnan(rbf_grad), zeros, rbf_grad)  # making sure there are no nans to avoid NaNs
+    # rbf_grad = rbf_grad.sum(-1)
+
     # print('rbf_deriv', rbf_grad)
-    print('coords.shape', coords.shape)
-    gto_l = torch.sum(rbf * sph, dim=(-2, -1)) 
+    # print('coords.shape', coords.shape)
+    gto_l = torch.sum(rbf * sph, dim=(-2, -1))
     gto_autograd = 0
-    for i in range(3):
+    for i in range(gto_l.squeeze().shape[0]):
         gto_autograd += torch.autograd.grad(gto_l.squeeze()[i], coords, retain_graph=True)[0]
-    print('rbf shape', rbf.shape)
-    print('rbf grad shape', rbf_grad.shape)
-    print('rbf grad unsqueeze shape', rbf_grad.unsqueeze(-2).shape)
-    print('sph shape', sph.shape)
-    print('sph deriv unsqueeze coords', sph_grad_c.unsqueeze(-1).shape) 
-    print('rbf_grad unsqueeze * sph', (rbf_grad.unsqueeze(-2) * sph).shape)
-    print('rbf* sph_grad unsqueeze ', (rbf * sph_grad_c.unsqueeze(-1)).shape)
-    gto_deriv = torch.sum(rbf_grad.unsqueeze(-2) * sph, -2) + rbf.squeeze(-1) * sph_grad_c
+    # print('rbf shape', rbf.shape)
+    # print('rbf grad shape', rbf_grad.shape)
+    # print('rbf grad unsqueeze shape', rbf_grad.unsqueeze(-2).shape)
+    # print('sph shape', sph.shape)
+    # print('sph deriv unsqueeze coords', sph_grad_c.unsqueeze(-1).shape)
+    # print('rbf_grad unsqueeze * sph', (rbf_grad.unsqueeze(-2) * sph).shape)
+    # print('rbf* sph_grad unsqueeze ', (rbf * sph_grad_c.unsqueeze(-1)).shape)
+    print('sph_grad_c', sph_grad_c.shape)
+    print('rbf grad', rbf_grad.shape)
+    print('rbf grad * sph', (rbf_grad.unsqueeze(-2) * sph.unsqueeze(-3)).shape)
+    print('rbf * sph grad', (rbf.unsqueeze(-3) * sph_grad_c.unsqueeze(-1).unsqueeze(-1)).shape)
+    gto_deriv = (rbf_grad.unsqueeze(-2) * sph.unsqueeze(-3)).sum((-2))\
+                + (rbf.unsqueeze(-3) * sph_grad_c.unsqueeze(-1).unsqueeze(-1)).sum((-2))
+    gto_deriv = gto_deriv.sum((-1))
+    print('gto_deriv.shape', gto_deriv.shape)
+    # print('sph_grad_c', sph_grad_c)
+    # print('rbf', rbf)
+    # print('sph', sph)
     print('gto autograd', gto_autograd)
     print('gto deriv', gto_deriv)
-    gto += gto_l 
+    gto += gto_l
     gto_grad += gto_deriv
 
 # grad = torch.autograd.grad(gto[0,1], coords, retain_graph=True)
 # print(grad)
 grad = 0
-for i in range(3):
+for i in range(gto.squeeze().shape[0]):
     grad += torch.autograd.grad(gto.squeeze()[i], coords, retain_graph=True)[0]
 print('gto', gto)
 print('total gto coords grad', grad)

src/equiv_dens/scripts/evaluate_density_errors.py
@@ -4,6 +4,7 @@ import argparse
 
 import torch
 from equiv_dens.training.parse_command_line_arguments import parse_command_line_arguments
+from pyscf.dft import numint
 from equiv_dens.data.density_dataset import AtomsDensityData
 from equiv_dens.utils.grids import cubical_grid, cubical_sampling, \
      CubicalGrid, spherical_grid, spherical_radial_sampling
@@ -19,6 +20,13 @@ import numpy as np
 import equiv_dens.training.utils as train_utils
 
 
+def str2bool(s):
+    if s.lower() in ('true', 't', '1'):
+        return True
+    elif s.lower() in ('false', 'f', '0'):
+        return False
+    else:
+        return s
 # %%
 def calc_density_errors(density, atoms, error_dict):
     r_dens = torch.clamp(density, min=0)
@@ -35,7 +43,7 @@ def calc_density_errors(density, atoms, error_dict):
     dpm2_error = 4.8 * torch.norm(dpm - atoms['dipole_moment'])
 
     dpm_mag_error = 4.8 * torch.mean(torch.abs(torch.linalg.norm(dpm) - torch.linalg.norm(atoms['dipole_moment'])))
-    dpm_ang_error = (180 / torch.pi) * torch.acos(torch.mean(torch.sum(dpm*atoms['dipole_moment']) /
+    dpm_ang_error = (180 / torch.pi) * torch.acos(torch.mean(torch.sum(dpm * atoms['dipole_moment']) /
                                                   (torch.linalg.norm(dpm) * torch.linalg.norm(atoms['dipole_moment']))))
 
     df43_error = torch.sum(torch.abs(atoms['density']**(4/3) -
@@ -88,6 +96,26 @@ def calc_density_errors(density, atoms, error_dict):
     error_dict['dpm_norm_error'].append(float(dpm_norm_error))
     error_dict['kl_loss'].append(float(kl_error))
     # print('i', i, 'mae', df_error, 'rmse', df2_error, 'dpm', dpm_error, 'mag', dpm_mag_error, 'ang', dpm_ang_error, 'lda', lda_error)
+
+
+def calc_density_grad_errors(density, atoms, error_dict):
+    dens_true = torch.permute(torch.cat([atoms['density'].unsqueeze(-1), atoms['density_grad']], dim=-1), (2, 1, 0)).squeeze()
+    ni = numint.NumInt()
+    exc_eff_pbe = torch.from_numpy(ni.eval_xc_eff('pbe', dens_true.numpy(force=True).astype(np.double), deriv=1, xctype='GGA')[0])
+    dens_calc = dens_true[0] * atoms['coord_weights']
+    # print('exchange correlation', exc_eff_pbe.shape)
+    exc_pbe = torch.sum(exc_eff_pbe * dens_calc)
+    exc_eff_pbe = torch.from_numpy(ni.eval_xc_eff('pbe', density.numpy(force=True).astype(np.double), deriv=1, xctype='GGA')[0])
+    dens_calc = density[0] * atoms['coord_weights']
+    # print('exchange correlation', exc_eff_pbe.shape)
+    exc_sum_pbe = torch.sum(exc_eff_pbe * dens_calc)
+    error_dict['exc_mae'].append(float(torch.abs(exc_sum_pbe - exc_pbe)))
+
+    vw_pred = torch.nansum(torch.norm(density[1:], dim=0)**2 / density * atoms['coord_weights'].squeeze())
+    vw_true = torch.nansum(torch.norm(dens_true[1:], dim=0)**2 / dens_true * atoms['coord_weights'].squeeze())
+    error_dict['vw_mae'].append(float(torch.abs(vw_pred - vw_true)))
+    error_dict['grad_norm_err'].append(float(torch.sum(torch.norm(density[1:] - dens_true[1:], dim=0) * atoms['coord_weigts'].squeeze())
+                                             / torch.sum(atoms['atom_numbers'])))
 
 # %%
 def make_plots(dens_ml, dens_df, sample_ref, save_name):
@@ -432,7 +460,10 @@ parser.add_argument('ref_np_load_file', type=str)
 parser.add_argument('ref_dens_load_file', type=str)
 parser.add_argument('res_load_file', type=str)
 parser.add_argument('save_file', type=str)
+parser.add_argument('--ml_error', type=str2bool, default=True)
 parser.add_argument('--df_error', action='store_true', default=False)
+parser.add_argument('--free_atom_error', action='store_true', default=False)
+parser.add_argument('--density_grad_error', action='store_true', default=False)
 parser.add_argument('--use_gpu', action='store_true', default=False)
 parser.add_argument('--num_samples', type=int, default=-1)
 parser.add_argument('--make_plots', action='store_true', default=False)
@@ -490,6 +521,7 @@ dataset = AtomsDensityData(np_path=args.np_dataset_test, density_path=args.dens_
                            atom_dens_path=args.atom_dens_path,
                            atom_dens_type=args.atom_dens_type,
                            projected_density=False,
+                           density_grad=main_args.density_grad_error,
                            )
 
 
@@ -499,13 +531,6 @@ if main_args.num_samples < 1:
 print('num samples', main_args.num_samples)
 df_losses = None
 if main_args.df_error:
-    df_losses = {'dens_mae': [], 'dens_rmse': [], 'dpm_mae': [],
-                 'dpm_rmse': [], 'kl_loss': [],
-                 'dpm_mag': [], 'dpm_ang': [], 'lda_mae': [],
-                 'coulomb': [], 'coulomb_int': [], 'mae_23': [], 'mae_43': [],
-                 'lda_23_mae': [], 'dpm_coord_rmse': [],
-                 'dpm_pos_coord_rmse': [], 'dpm_neg_coord_rmse': [],
-                 'dpm_int_rmse': [], 'dpm_norm_error': []}
 
     dataset_df = AtomsDensityData(np_path=args.np_dataset_test, density_path=args.dens_dataset_test,
                                   orbitals_path=args.orbitals_file,
@@ -528,13 +553,24 @@ if main_args.df_error:
                                   atom_dens_path=args.atom_dens_path,
                                   atom_dens_type=args.atom_dens_type,
                                   projected_density=True,
+                                  density_grad=main_args.density_grad_error,
                                   )
-    for i in range(min(len(dataset), main_args.num_samples)):
-        sample = dataset.get_properties([i])
-        sample_df = dataset_df.get_properties([i])
-        print('density integral', torch.sum(sample_df['density'] * sample['coord_weights']))
-        calc_density_errors(sample_df['density'], sample, df_losses)
-        # print('i', i, 'mae', df_error, 'rmse', df2_error, 'dpm', dpm_error, 'mag', dpm_mag_error, 'ang', dpm_ang_error, 'lda', lda_error)
+    if not main_args.density_grad_error:
+        df_losses = {'dens_mae': [], 'dens_rmse': [], 'dpm_mae': [],
+                     'dpm_rmse': [], 'kl_loss': [],
+                     'dpm_mag': [], 'dpm_ang': [], 'lda_mae': [],
+                     'coulomb': [], 'coulomb_int': [], 'mae_23': [], 'mae_43': [],
+                     'lda_23_mae': [], 'dpm_coord_rmse': [],
+                     'dpm_pos_coord_rmse': [], 'dpm_neg_coord_rmse': [],
+                     'dpm_int_rmse': [], 'dpm_norm_error': []}
+        for i in range(min(len(dataset), main_args.num_samples)):
+            sample = dataset.get_properties([i])
+            sample_df = dataset_df.get_properties([i])
+            print('density integral', torch.sum(sample_df['density'] * sample['coord_weights']))
+            calc_density_errors(sample_df['density'], sample, df_losses)
+    # else:
+    #     df_losses = {'exc_mae': [], 'vw_mae': [], 'grad_norm_err': []}
+    #     # print('i', i, 'mae', df_error, 'rmse', df2_error, 'dpm', dpm_error, 'mag', dpm_mag_error, 'ang', dpm_ang_error, 'lda', lda_error)
 
     np.save('datasets/' + main_args.save_file + '_df_losses.npy', df_losses, allow_pickle=True)
     print('DF losses')
@@ -542,38 +578,60 @@ if main_args.df_error:
         print(key)
         print(np.nanmean(df_losses[key]))
 
-# %%
-res_dataset = torch.load(main_args.res_load_file, map_location='cpu')
-print('res dataset length', len(res_dataset))
-print('res sample pos shape', res_dataset['positions'].shape)
-print('res sample dens shape', res_dataset['density'].shape)
-res_losses = {'dens_mae': [], 'dens_rmse': [], 'dpm_mae': [],
-              'dpm_rmse': [], 'kl_loss': [],
-              'dpm_mag': [], 'dpm_ang': [], 'lda_mae': [],
-              'coulomb': [], 'coulomb_int': [], 'mae_23': [], 'mae_43': [],
-              'lda_23_mae': [], 'dpm_coord_rmse': [],
-              'dpm_pos_coord_rmse': [], 'dpm_neg_coord_rmse': [],
-              'dpm_int_rmse': [], 'dpm_norm_error': []}
-for i in range(min(len(dataset), main_args.num_samples)):
-    # print('sample pdist', torch.cdist(sample['positions'], sample['positions'])[0, :3,:3])
-    # print('res_pdist', torch.cdist(res_dataset['positions'][[i]], res_dataset['positions'][[i]])[0, :3,:3])
-    sample = dataset.get_properties([i])
-    # print('sample positions', sample['positions'])
-    # print('res dataset_positions', res_dataset['positions'][[i]])
-    # print('density integral', torch.sum(res_dataset['density'][[i]] * sample['coord_weights']))
-    calc_density_errors(res_dataset['density'][[i]], sample, res_losses)
+if main_args.free_atom_error:
+    free_atom_losses = {'dens_mae': [], 'dens_rmse': [], 'dpm_mae': [],
+                        'dpm_rmse': [], 'kl_loss': [],
+                        'dpm_mag': [], 'dpm_ang': [], 'lda_mae': [],
+                        'coulomb': [], 'coulomb_int': [], 'mae_23': [], 'mae_43': [],
+                        'lda_23_mae': [], 'dpm_coord_rmse': [],
+                        'dpm_pos_coord_rmse': [], 'dpm_neg_coord_rmse': [],
+                        'dpm_int_rmse': [], 'dpm_norm_error': []}
 
-np.save('datasets/' + main_args.save_file + '.npy', res_losses, allow_pickle=True)
-print('Results losses')
-for key in res_losses.keys():
-    print(key)
-    print(np.nanmean(res_losses[key]), np.nanmean(df_losses[key]) if df_losses is not None else "")
-print('true dens max', torch.max(sample['density']))
-print('ML dens max', torch.max(res_dataset['density'][[i]]))
-if main_args.df_error:
-    print('DF dens max', torch.max(sample_df['density']))
-    print('dpm pos neg diff', np.nanmean(res_losses['dpm_pos_coord_rmse']) - np.nanmean(res_losses['dpm_neg_coord_rmse']),
-        np.nanmean(df_losses['dpm_pos_coord_rmse']) - np.nanmean(df_losses['dpm_neg_coord_rmse']))
+    for i in range(min(len(dataset), main_args.num_samples)):
+        sample = dataset.get_properties([i])
+        print('density integral', torch.sum(sample['atom_density'] * sample['coord_weights']))
+        calc_density_errors(sample['atom_density'], sample, free_atom_losses)
+        # print('i', i, 'mae', df_error, 'rmse', df2_error, 'dpm', dpm_error, 'mag', dpm_mag_error, 'ang', dpm_ang_error, 'lda', lda_error)
+
+    np.save('datasets/' + main_args.save_file + '_free_atom_losses.npy', free_atom_losses, allow_pickle=True)
+    print('DF losses')
+    for key in free_atom_losses.keys():
+        print(key)
+        print(np.nanmean(free_atom_losses[key]))
+
+# %%
+if main_args.ml_error:
+    res_dataset = torch.load(main_args.res_load_file, map_location='cpu')
+    print('res dataset length', len(res_dataset))
+    print('res sample pos shape', res_dataset['positions'].shape)
+    print('res sample dens shape', res_dataset['density'].shape)
+    res_losses = {'dens_mae': [], 'dens_rmse': [], 'dpm_mae': [],
+                  'dpm_rmse': [], 'kl_loss': [],
+                  'dpm_mag': [], 'dpm_ang': [], 'lda_mae': [],
+                  'coulomb': [], 'coulomb_int': [], 'mae_23': [], 'mae_43': [],
+                  'lda_23_mae': [], 'dpm_coord_rmse': [],
+                  'dpm_pos_coord_rmse': [], 'dpm_neg_coord_rmse': [],
+                  'dpm_int_rmse': [], 'dpm_norm_error': []}
+    for i in range(min(len(dataset), main_args.num_samples)):
+        # print('sample pdist', torch.cdist(sample['positions'], sample['positions'])[0, :3,:3])
+        # print('res_pdist', torch.cdist(res_dataset['positions'][[i]], res_dataset['positions'][[i]])[0, :3,:3])
+        sample = dataset.get_properties([i])
+        # print('sample positions', sample['positions'])
+        # print('res dataset_positions', res_dataset['positions'][[i]])
+        # print('density integral', torch.sum(res_dataset['density'][[i]] * sample['coord_weights']))
+        calc_density_errors(res_dataset['density'][[i]], sample, res_losses)
+
+    np.save('datasets/' + main_args.save_file + '.npy', res_losses, allow_pickle=True)
+    print('Results losses')
+    for key in res_losses.keys():
+        print(key)
+        print(np.nanmean(res_losses[key]), np.nanmean(df_losses[key]) if df_losses is not None else "")
+    print('true dens max', torch.max(sample['density']))
+    print('ML dens max', torch.max(res_dataset['density'][[i]]))
+    if main_args.df_error:
+        print('DF dens max', torch.max(sample_df['density']))
+        print('dpm pos neg diff', np.nanmean(res_losses['dpm_pos_coord_rmse']) - np.nanmean(res_losses['dpm_neg_coord_rmse']),
+              np.nanmean(df_losses['dpm_pos_coord_rmse']) - np.nanmean(df_losses['dpm_neg_coord_rmse']))
 
 # %%
 if main_args.make_plots and main_args.df_error:

src/equiv_dens/utils/spherical_harmonics_deriv.py
@@ -86,12 +86,13 @@ def spherical_harmonics_deriv(L, u):
 
 # spherical harmonics of order 0
 def Y0_deriv(u):
-    return u.new_zeros((*u.shape[:-1], 1))
+    return u.new_zeros((*u.shape[:-1], 1, u.shape[-1]))
 
 # spherical harmonics of order 1
 sqrt3 = np.sqrt(3)
 def Y1_deriv(dx, dy, dz):
-    return sqrt3 * (dx + dy + dz)
+    # return (sqrt3 / 3) * (dx + dy + dz).unsqueeze(-1).expand(-1, -1, -1, 3)
+    return sqrt3 * torch.stack((dy, dz,dx), dim=-2)
 # # spherical harmonics of order 2
 sqrt15 = np.sqrt(15)
 sqrt5over2 = np.sqrt(5) / 2
@@ -106,7 +107,7 @@ def Y2_deriv(dxy, dyz, dxz, _d3z2m1, _dx2my2):
             sqrt5over2 * _d3z2m1,
             sqrt15 * dxz,
             sqrt15over2 * _dx2my2,
-        ), dim=-1)
+        ), dim=-2)
 
 
 # spherical harmonics of order 3
@@ -131,7 +132,7 @@ def Y3_deriv(x, y, z, z2, _x2my2, _3x2my2, _x2m3y2,
             sqrt105over2 * (z * _dx2my2 + dz * _x2my2),
             sqrt70over4 * (dx * _x2m3y2 + x * _dx2m3y2),
         ),
-        dim=-1,
+        dim=-2,
     )
 #
 #
@@ -164,7 +165,7 @@ def Y4_deriv(x2, y2, z2, xy, yz, xz, _x2my2, _x2m3y2,
             sqrt70_3over4 * (dxz * _x2m3y2 + xz * _dx2m3y2),
             sqrt35_3over8 * (dx4 - 6 * dx2y2 + dy4),
         ),
-        dim=-1,
+        dim=-2,
     )
 
 # spherical harmonics of order 5
@@ -205,9 +206,9 @@ def Y5_deriv(x, y, z, z2, xyz, x4, y4, x2y2,
                              x * (_dx2m3y2 * _9z2m1 + _x2m3y2 * _d9z2m1)),
             sqrt385_3over8 * (dz * (x4 - 6 * x2y2 + y4) + z * (dx4 - 6 * dx2y2 + dy4)),
             sqrt154_3over16 * (dx * (x4 - 10 * x2y2 + 5 * y4) +
-                              x * (dx4 - 10 * dx2y2 + 5 * dy4)),
+                               x * (dx4 - 10 * dx2y2 + 5 * dy4)),
         ),
-        dim=-1,
+        dim=-2,
     )
 #
 #