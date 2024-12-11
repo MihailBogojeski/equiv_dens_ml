ba2037e2ea66eafaa8f0d74b68e0796dab014eb3
Mihail Bogojeski <mihailbogojeski@gmail.com>
2024-12-11 08:50:57 -0800

    Adjusted schnetpack trajectory to MD file

--- src/equiv_dens/scripts/schnetpack_md_to_npy.py
+++ src/equiv_dens/scripts/schnetpack_md_to_npy.py
@@ -6,13 +6,16 @@ import equiv_dens.utils.base as utils
 
 file = sys.argv[1]
 every = int(sys.argv[2])
+n_runs = int(sys.argv[3])
 
 data = HDF5Loader(file, load_properties=False)
 positions = data.properties['_positions'][::every] * 10
 atom_numbers = data.properties['_atomic_numbers']
+positions = np.reshape(positions, (positions.shape[0], n_runs, -1, positions.shape[-1]))
+atom_numbers = np.reshape(atom_numbers, (n_runs, -1))
 print('positions.shape', positions.shape)
 print('atom_numbers.shape', atom_numbers.shape)
 
 for i in range(positions.shape[1]):
-    atoms = {'positions': positions[:, i], 'atom_numbers': atom_numbers}
+    atoms = {'positions': positions[:, i], 'atom_numbers': atom_numbers[i, :]}
     np.save(file[:-5] + '_' + str(i) + '.npy', atoms)
