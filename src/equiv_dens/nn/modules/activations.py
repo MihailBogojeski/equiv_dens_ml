035dc029589ebda52e55c9dfb82074b08d9e7528
Nico Kestel <=>
2024-12-14 19:03:30 +0100

    NormGate

--- src/equiv_dens/nn/modules/activations.py
+++ src/equiv_dens/nn/modules/activations.py
@@ -51,3 +51,59 @@ class Swish(nn.Module):
 
     def forward(self, x):
         return self.alpha * x * torch.sigmoid(self.beta * x)
+
+
+class NormGate(nn.Module):
+    """
+    QHNet's NormGate (https://arxiv.org/abs/2306.04922)
+    """
+
+    def __init__(self, num_features, max_order, mlp_activation="swish"):
+        super(NormGate, self).__init__()
+
+        self.num_features = num_features
+        self.max_order = max_order
+        
+        if mlp_activation == "swish":
+            self.mlp_activation = Swish((self.max_order + 1) * self.num_features)
+        elif mlp_activation == "ssp":
+            self.mlp_activation = ShiftedSoftplus((self.max_order + 1) * self.num_features)
+        else:
+            raise ValueError(f"Unsupported activation function: {mlp_activation}")
+
+        self.mlp = nn.Sequential(
+            nn.Linear((self.max_order + 1) * self.num_features, (self.max_order + 1) * self.num_features),
+            self.mlp_activation,
+            nn.Linear((self.max_order + 1) * self.num_features, (self.max_order + 1) * self.num_features))
+
+    def forward(self, x):
+
+        # dimensions for reshaping mlp output
+        bs, n_atoms, _, num_features = x[0].shape
+        degrees = len(x)
+
+        norms = [torch.norm(x[i], dim=-2, keepdim=True) for i in range(1, len(x))]
+        norms = [x[0]] + norms
+        norms = torch.cat(norms, dim=-2)
+        # print("norms", norms.shape)
+
+        norms = norms.view(bs, n_atoms, -1)
+        # print("norms (flattened)", norms.shape)
+
+        mlp_norms = self.mlp(norms)
+        # print("mlp_norms", mlp_norms.shape)
+
+        mlp_norms = mlp_norms.view(bs, n_atoms, degrees, num_features)
+        # print("mlp_norms (reshaped)", mlp_norms.shape)
+
+        # print(f"x[0]: {x[0].shape}")
+        # print(f"x[1]: {x[1].shape}")
+        # print(f"x[2]: {x[2].shape}")
+        # print(f"mlp_norms: {mlp_norms.shape}")
+
+        # l=0 mlp output is not multiplied with l=0 input
+        x_norm = [mlp_norms[:, :, [0]]] + [x[i] * mlp_norms[:, :, [i]] for i in range(1, len(x))]
+
+        # print("x_norm", len(x_norm), x_norm[0].shape, x_norm[-1].shape)
+
+        return x_norm
