1bfe65f951774c41378f52595ccd1d641d6d0a21
MihailBogojeski <mihail.bogojeski@campus.tu-berlin.de>
2025-04-18 05:19:37 +0200

    adding velocities to schnetpack md load

--- src/equiv_dens/scripts/schnetpack_md_to_npy.py
+++ src/equiv_dens/scripts/schnetpack_md_to_npy.py
@@ -6,13 +6,19 @@ import equiv_dens.utils.base as utils
 
 file = sys.argv[1]
 every = int(sys.argv[2])
+n_traj = int(sys.argv[3])
 
 data = HDF5Loader(file, load_properties=False)
 positions = data.properties['_positions'].squeeze(1)[::every] * 10
+positions = np.reshape(positions, (positions.shape[0], n_traj, -1, positions.shape[-1]))
+velocities = data.properties['velocities'].squeeze(1)[::every] * 10
+velocities = np.reshape(velocities, (velocities.shape[0], n_traj, -1, velocities.shape[-1]))
 atom_numbers = data.properties['_atomic_numbers']
+atom_numbers = np.reshape(atom_numbers, (n_traj, -1))
 print('positions.shape', positions.shape)
+print('velocities.shape', velocities.shape)
 print('atom_numbers.shape', atom_numbers.shape)
 
 for i in range(positions.shape[1]):
-    atoms = {'positions': positions[:, i], 'atom_numbers': atom_numbers}
-    np.save(file[:-5] + '_' + str(i) + '.npy', atoms)
+    atoms = {'positions': positions[:, i], 'atom_numbers': atom_numbers[i], 'velocities': velocities[:, i]}
+    np.save(file[:-5] + '_vel_' + str(i) + '.npy', atoms)
